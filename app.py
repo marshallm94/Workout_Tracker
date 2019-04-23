fd5aee68e10bc93610f66340e133cc333dc28a21
Marshall McQuillen <marshallm94@gmail.com>
2019-04-22 21:00:22 -0600

    transition index and button options to HTML

--- app.py
+++ app.py
@@ -26,21 +26,45 @@ class OptionForm(FlaskForm):
 def index():
 
 	form = OptionForm()
+	
+	if request.method == 'POST':
 
-	if 'create_workout' in request.form:
-		print('creating workout')
-	elif 'start_workout' in request.form:
-		print('starting workout')
-	elif 'check_progress' in request.form:
-		print('checking progress')
+		if 'create_workout' in request.form:
 
-	return render_template('index2.html', form=form)
+			print('creating workout')
+
+		elif 'start_workout' in request.form:
+
+			print('starting workout')
+
+		elif 'check_progress' in request.form:
+
+			print('checking progress')
+	
+	elif request.method == 'GET':
+
+		pass
+
+	return render_template('index.html', form=form)
 
 
-@app.route('/user/<name>')
-def user(name):
+@app.route('/start_workout')
+def start_workout():
 
-	return render_template('user.html', name=name)
+	return render_template('start_workout.html')
+
+
+@app.route('/create_workout')
+def create_workout():
+
+	return render_template('create_workout.html')
+
+
+@app.route('/check_progress')
+def check_progress():
+
+	return render_template('check_progress.html')
+
 
 if __name__ == "__main__":
 
