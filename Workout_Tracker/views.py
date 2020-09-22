c207bee0e5d918409dd8c3bc9c32dc8ec1b50958
Marshall McQuillen <marshallm94@gmail.com>
2020-09-22 13:08:07 -0500

    Add note about start workout functionality

--- Workout_Tracker/views.py
+++ Workout_Tracker/views.py
@@ -8,6 +8,9 @@ import json
 data_directory = os.path.join("Workout_Tracker", "data/")
 examples_directory = os.path.join("Workout_Tracker", "examples/")
 
+########################################
+# JSON file loads (for development phase) 
+########################################
 with open(os.path.join(data_directory, 'display_mesocycle.json')) as f:
     display_meso = json.load(f)
 with open(os.path.join(data_directory, 'stage1.json')) as f:
@@ -16,29 +19,17 @@ with open(os.path.join(data_directory, 'stage2.json')) as f:
     stage_2 = json.load(f)
 with open(os.path.join(data_directory, 'stage3.json')) as f:
     stage_3 = json.load(f)
-
 with open(os.path.join(examples_directory, 'progression_model.json')) as f:
     progression_example = json.load(f)
 
 @app.route('/', methods=['GET','POST'])
 def index():
 
-    #return render_template('index.html',tmp=exercise_table)
     return render_template('index.html')
 
-@app.route('/review_mesocycle', methods=['GET','POST'])
-def review_mesocycle():
-
-    meso_length = 3
-    return render_template('review_mesocycle.html',
-            split = display_meso,
-            meso_length = meso_length)
-
-@app.route('/continue_mesocycle')
-def continue_mesocycle():
-
-    return render_template('continue_mesocycle.html')
-
+########################################
+# Mesocycle Creation Views
+########################################
 @app.route('/create_mesocycle', methods=['GET','POST'])
 def create_mesocycle():
 
@@ -83,6 +74,63 @@ def create_periodization_models():
             models = stage_3,
             example = progression_example)
 
+@app.route('/review_mesocycle', methods=['GET','POST'])
+def review_mesocycle():
+
+    meso_length = 3
+    return render_template('review_mesocycle.html',
+            split = display_meso,
+            meso_length = meso_length)
+
+########################################
+# Start Previously Defined Mesocycle Views
+########################################
+@app.route('/start_mesocycle', methods=['GET','POST'])
+def start_mesocycle():
+
+    if request.method == 'GET':
+        meso_length = 3
+        print(display_meso)
+        return render_template('start_mesocycle.html',
+                split = display_meso,
+                meso_length = meso_length)
+
+    # NOTE: Will this method ever actually need POST functionality? Wouldn't
+    # the user simply be choosing from previously defined mesocycles?
+    #   (2020-09-22): I believe this method will not need POST functionality
+    elif request.method == 'POST':
+        pass 
+
+# NOTE(2020-09-22): If this route is called, the user has selected a meso to
+# start. Therefore, the backend logic (at some point) needs to determine:
+#
+#       - what day in the split it is
+#       - what microcycle within the mesocycle it is
+#
+# I think the best way to keep things orthogonal is to perform all this logic in
+# a separate function, and then pass a "todays-workout" object to this function.
+# This way, this function can be abstract enough to accommodate for any number
+# of exercises, sets, reps, etc. In addition, it can remain simple enough to not
+# clutter this file, as well as the concept that the view should be an
+# intermediary - front end <--- views (this file) ---> back end.
+@app.route('/start_workout', methods=['GET','POST'])
+def start_workout():
+
+    if request.method == 'GET':
+
+        return render_template('start_workout.html')
+
+    elif request.method == 'POST':
+        pass
+
+    #return render_template('start_workout.html')
+
+
+@app.route('/continue_mesocycle')
+def continue_mesocycle():
+
+    return render_template('continue_mesocycle.html')
+
 @app.route('/create_one_off_workout')
 def create_workout():
 
@@ -93,10 +141,3 @@ def create_workout():
 def check_progress():
 
     return render_template('check_progress.html')
-
-
-@app.route('/start_one_off_workout', methods=['GET','POST'])
-def start_workout():
-
-
-    return render_template('start_workout.html')
---
+++ install_and_run_app.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=Workout_Tracker
+export FLASK_ENV=development
+pip3 install -e .
+flask run
